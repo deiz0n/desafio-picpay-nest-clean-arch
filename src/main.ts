d04862ede8fb24483d14f618f7da511ef911c1ab
deiz0n <eduardomacedosilva@outlook.com>
2026-04-30 17:25:44 -0300

    chore: initial setup

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1.0');
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('API_PORT') || 3000;
+
+  await app.listen(port);
+}
+void bootstrap();

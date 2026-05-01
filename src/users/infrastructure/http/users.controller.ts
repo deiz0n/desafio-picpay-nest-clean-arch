b029a76ce16c85c325e0d063003649a48f9b9664
deiz0n <eduardomacedosilva@outlook.com>
2026-05-01 19:11:03 -0300

    feat: add create and getAll endpoints

--- src/users/infrastructure/http/users.controller.ts
+++ src/users/infrastructure/http/users.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Inject,
+  Post,
+  Query,
+} from '@nestjs/common';
+import type { CreateUserUseCase } from '../../core/use-cases/create-user.use-case';
+import type { GetAllUsersUseCase } from '../../core/use-cases/get-all-users.use-case';
+import { CreateUserDto } from '../dtos/CreateUserDto';
+import { PaginationDto } from '../dtos/PaginationDto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    @Inject('CreateUserUseCase')
+    private readonly createUserUseCase: CreateUserUseCase,
+    @Inject('GetAllUsersUseCase')
+    private readonly getAllUsersUseCase: GetAllUsersUseCase,
+  ) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createDto: CreateUserDto) {
+    const result = await this.createUserUseCase.execute(createDto);
+    return {
+      status: HttpStatus.CREATED,
+      timestamp: new Date().toISOString(),
+      data: result,
+    };
+  }
+
+  @Get()
+  async getAll(@Query() query: PaginationDto) {
+    const result = await this.getAllUsersUseCase.execute(
+      query.page,
+      query.pageSize,
+    );
+    return {
+      status: HttpStatus.CREATED,
+      timestamp: new Date().toISOString(),
+      data: result,
+    };
+  }
+}

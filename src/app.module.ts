385a1af25647284b88fb29f767ca6cc587734994
deiz0n <eduardomacedosilva@outlook.com>
2026-05-01 17:50:10 -0300

    fix: rename drizzle module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-import { DrizzleModule } from './drizzle/drizlle.module';
+import { DrizzleModule } from './drizzle/drizzle.module';
 
 @Module({
   imports: [

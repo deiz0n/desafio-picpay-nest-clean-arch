0bdd105a4f37b61d7ab01b9a37ae81655f427692
deiz0n <eduardomacedosilva@outlook.com>
2026-05-01 20:05:38 -0300

    chore: add transactions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { DrizzleModule } from './drizzle/drizzle.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
     DrizzleModule,
+    TransactionsModule,
   ],
   controllers: [AppController],
   providers: [],

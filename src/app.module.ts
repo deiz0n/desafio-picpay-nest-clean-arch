550b542670a2bef3e1bf11d4d712709e09562a51
deiz0n <eduardomacedosilva@outlook.com>
2026-05-01 17:34:20 -0300

    chore: import DrizzleModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { DrizzleModule } from './drizzle/drizlle.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    DrizzleModule,
+  ],
   controllers: [AppController],
   providers: [],
 })

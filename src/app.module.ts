abe55c60e35bbda6f36864a3e2a1f062afc48da1
deiz0n <eduardomacedosilva@outlook.com>
2026-05-04 19:40:51 -0300

    feat: add accounts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { TransactionsModule } from './transactions/transactions.module';
     UsersModule,
     DrizzleModule,
     TransactionsModule,
+    AccountsModule,
   ],
   controllers: [AppController],
   providers: [],

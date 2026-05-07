4a737fc216b4312c8350f493e482b700e551906c
deiz0n <eduardomacedosilva@outlook.com>
2026-05-07 14:26:15 -0300

    feat: add endpoint create new transactions

--- src/transactions/infrastructure/http/transactions.controller.ts
+++ src/transactions/infrastructure/http/transactions.controller.ts
@@ -1,6 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Inject,
+  Post,
+} from '@nestjs/common';
+import type { SendTransactionUseCase } from '../../aplication/use-cases/send-transaction.use-case';
+import { CreateTransactionDto } from '../dtos/CreateTransactionDto';
 
 @Controller('transactions')
 export class TransactionsController {
-  constructor() {}
+  constructor(
+    @Inject('SendTransactionUseCase')
+    private readonly sendTransactionUseCase: SendTransactionUseCase,
+  ) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createDto: CreateTransactionDto) {
+    const result = await this.sendTransactionUseCase.execute(createDto);
+    return {
+      status: HttpStatus.CREATED,
+      timestamp: new Date().toISOString(),
+      data: result,
+    };
+  }
 }

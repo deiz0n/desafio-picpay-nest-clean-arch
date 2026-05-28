bc9aafe5ef0a4475fd5a1f9b4d5bbf755f0e0bba
deiz0n <eduardomacedosilva@outlook.com>
2026-05-28 20:19:08 -0300

    fix: adjustment in validation in amount attribute

--- src/transactions/infrastructure/dtos/CreateTransactionDto.ts
+++ src/transactions/infrastructure/dtos/CreateTransactionDto.ts
@@ -3,8 +3,11 @@ import { z } from 'zod';
 
 const CreateTransactionSchema = z.object({
   payerId: z.uuid('Invalid UUID').nonempty('payerId is required'),
-  payeeId: z.uuid('Invalid UUID').nonempty('payerId is required'),
-  amount: z.number().positive('The value is positive or greater than 0s'),
+  payeeId: z.uuid('Invalid UUID').nonempty('payeeId is required'),
+  amount: z
+    .number()
+    .positive('The value is positive or greater than 0s')
+    .transform((vol) => Math.round(vol * 100)),
 });
 
 export class CreateTransactionDto extends createZodDto(

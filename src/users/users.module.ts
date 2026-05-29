47f9c9cbcc23b79ac208c25ea815702fee4febff
deiz0n <eduardomacedosilva@outlook.com>
2026-05-29 17:30:38 -0300

    chore: add hasher provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -10,11 +10,13 @@ import {
 import { AccountsRepositoryImpl } from '../accounts/infrastructure/repositories/accounts.repository';
 import { UpdateUserService } from './application/services/update-user.service';
 import { GetUserByIdService } from './application/services/get-user-by-id.service';
+import { SharedModule, HASHER_PROVIDER_TOKEN } from '../shared/shared.module';
+import { IHasherProvider } from '../shared/application/providers/hasher.provider';
 
 export const USER_REPOSITORY_TOKEN = 'USER_REPOSITORY_TOKEN';
 
 @Module({
-  imports: [AccountsModule],
+  imports: [AccountsModule, SharedModule],
   controllers: [UsersController],
   providers: [
     {
@@ -26,10 +28,11 @@ export const USER_REPOSITORY_TOKEN = 'USER_REPOSITORY_TOKEN';
       useFactory: (
         userRepepository: UserRepositoryImpl,
         accountsRepository: AccountsRepositoryImpl,
+        hasher: IHasherProvider,
       ) => {
-        return new CreateUserService(userRepepository, accountsRepository);
+        return new CreateUserService(userRepepository, accountsRepository, hasher);
       },
-      inject: [USER_REPOSITORY_TOKEN, ACCOUNT_REPOSITORY_TOKEN],
+      inject: [USER_REPOSITORY_TOKEN, ACCOUNT_REPOSITORY_TOKEN, HASHER_PROVIDER_TOKEN],
     },
     {
       provide: 'GetAllUsersUseCase',
@@ -47,10 +50,13 @@ export const USER_REPOSITORY_TOKEN = 'USER_REPOSITORY_TOKEN';
     },
     {
       provide: 'UpdateUserUseCase',
-      useFactory: (userRepository: UserRepositoryImpl) => {
-        return new UpdateUserService(userRepository);
+      useFactory: (
+        userRepository: UserRepositoryImpl,
+        hasher: IHasherProvider,
+      ) => {
+        return new UpdateUserService(userRepository, hasher);
       },
-      inject: [USER_REPOSITORY_TOKEN],
+      inject: [USER_REPOSITORY_TOKEN, HASHER_PROVIDER_TOKEN],
     },
   ],
   exports: [USER_REPOSITORY_TOKEN],

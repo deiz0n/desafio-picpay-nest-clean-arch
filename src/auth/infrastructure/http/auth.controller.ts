ac5c710ff66675bc9cf1fb766daa9d291c6a1905
deiz0n <eduardomacedosilva@outlook.com>
2026-06-12 22:41:27 -0300

    refactor: now token jwt is insert in cookie

--- src/auth/infrastructure/http/auth.controller.ts
+++ src/auth/infrastructure/http/auth.controller.ts
@@ -1,19 +1,48 @@
-import { Body, Controller, HttpCode, HttpStatus, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Inject,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { LoginDto } from './dtos/LoginDto';
 import type { LoginUseCase } from '../../core/use-cases/login.use-case';
 import { Public } from '../decorators/public.decorator';
+import type { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     @Inject('LoginUseCase')
     private readonly loginUseCase: LoginUseCase,
+    private readonly configService: ConfigService,
   ) {}
 
   @Public()
   @Post('login')
   @HttpCode(HttpStatus.OK)
-  async login(@Body() loginDto: LoginDto) {
-    return this.loginUseCase.execute(loginDto.email, loginDto.password);
+  async login(
+    @Body() loginDto: LoginDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const result = await this.loginUseCase.execute(
+      loginDto.email,
+      loginDto.password,
+    );
+
+    response.cookie('access_token', result.accessToken, {
+      httpOnly: true,
+      secure: this.configService.get<string>('NODE_ENV') === 'production',
+      sameSite: 'lax',
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+
+    return {
+      status: HttpStatus.OK,
+      data: result.user,
+    };
   }
 }

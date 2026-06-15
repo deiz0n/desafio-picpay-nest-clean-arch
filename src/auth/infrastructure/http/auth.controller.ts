af0f5e12fcfc7da3c81876a21fe6653a4bf6ca6a
deiz0n <eduardomacedosilva@outlook.com>
2026-06-15 17:02:42 -0300

    feat: add getProfile and logout endpoints

--- src/auth/infrastructure/http/auth.controller.ts
+++ src/auth/infrastructure/http/auth.controller.ts
@@ -1,17 +1,22 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Inject,
   Post,
+  Req,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { LoginDto } from './dtos/LoginDto';
 import type { LoginUseCase } from '../../core/use-cases/login.use-case';
-import { Public } from '../decorators/public.decorator';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import type { Response } from 'express';
 import { ConfigService } from '@nestjs/config';
+import { CurrentUser } from '../decorators/current-user.decorator';
+import type { UserResponse } from 'src/users/core/user-response';
 
 @Controller('auth')
 export class AuthController {
@@ -21,7 +26,6 @@ export class AuthController {
     private readonly configService: ConfigService,
   ) {}
 
-  @Public()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   async login(
@@ -44,5 +48,26 @@ export class AuthController {
       status: HttpStatus.OK,
       data: result.user,
     };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  @HttpCode(HttpStatus.OK)
+  async getProfile(@CurrentUser() user: UserResponse) {
+    return {
+      status: HttpStatus.OK,
+      data: user,
+    };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('logout')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async logout(@Res({ passthrough: true }) response: Response) {
+    response.clearCookie('access_token', {
+      httpOnly: true,
+      secure: this.configService.get<string>('NODE_ENV') === 'production',
+      sameSite: 'lax',
+    });
   }
 }
